747dd2bac47dbfe394ab06066a3336ab9d609f1c
= <cryptic.kh.ua@gmail.com>
2014-09-23 16:23:06 +0300

    support py2/3 (test version)

--- useful_stuff/crawler_py3.py
+++ useful_stuff/crawler_py3.py
@@ -1,16 +1,12 @@
 from bs4 import BeautifulSoup
 try:
-    import urllib2 as urllib2
+    import urllib2 as urllib
 except ImportError:
-    import urllib2
+    import urllib
 try:
     import http.cookiejar as cookielib
 except ImportError:
     import cookielib
-try:
-    from urllib.request import Request
-except ImportError:
-    from urllib2 import Request
 
 cj = cookielib.CookieJar()
 c1 = cookielib.Cookie(version=0, name='uid',
@@ -33,50 +29,17 @@ c2 = cookielib.Cookie(version=0, name='pass',
                       comment_url=None, rest={'HttpOnly': None})
 cj.set_cookie(c1)
 cj.set_cookie(c2)
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+opener = urllib.build_opener(urllib.HTTPCookieProcessor(cj))
 opener.addheaders = [('User-agent', 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)')]
 
-
+# for py2 need import urllib
 # values = {'name': 'Michael Ford',
 #           'location': 'Northampton',
 #           'language': 'Python'}
 # data = urllib.urlencode(values)
 
-response = opener.open('http://tracker.maxnet.ua/details.php?id=76590', None)
+response = opener.open('http://tracker.maxnet.ua/details.php?id=76590')
 soup = BeautifulSoup(response.read().decode('cp1251'))
 
 for link in soup.find_all('strong'):
     print(link)
-
-# if __name__ == "__main__":
-#     cj = http.cookiejar.CookieJar()
-#     c1 = http.cookiejar.Cookie(version=0, name='uid',
-#         value='7530',
-#         port=None, port_specified=False,
-#         domain='tracker.maxnet.ua',
-#         domain_specified=True,
-#         domain_initial_dot=True, path='/',
-#         path_specified=True, secure=False,
-#         expires=None, discard=True, comment=None,
-#         comment_url=None, rest={'HttpOnly': None})
-#     c2 = http.cookiejar.Cookie(version=0, name='pass',
-#         value='bfd543fc40cd9e953845fd4239086255',
-#         port=None, port_specified=False,
-#         domain='tracker.maxnet.ua',
-#         domain_specified=True,
-#         domain_initial_dot=True, path='/',
-#         path_specified=True, secure=False,
-#         expires=None, discard=True, comment=None,
-#         comment_url=None, rest={'HttpOnly': None})
-#
-#     cj.set_cookie(c1)
-#     cj.set_cookie(c2)
-#     opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-#
-#     headers =  {'User-agent' : 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
-#     request = urllib.request.Request('http://tracker.maxnet.ua/details.php?id=76590', None, headers)
-#     res = opener.open(request)
-#     soup = BeautifulSoup(res.read().decode('cp1251'))
-#
-#     for link in soup.find_all('strong'):
-#         print(link)
