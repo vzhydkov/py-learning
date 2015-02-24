eb72e8f74586c6d19c6e00b283f820584092caec
vzhidkov <cryptic.kh.ua@gmail.com>
2015-02-24 22:26:47 +0200

    +singleton

--- useful_stuff/meta.py
+++ useful_stuff/meta.py
@@ -2,25 +2,27 @@
 # class = type("classname", superclasses, attributedict)
 # NewType = type("NewType", (object,), {"x": "hello"})
 
+
 def with_metaclass(meta, *bases):
     """Create a base class with a metaclass."""
     return meta("NewBase", bases, {})
 
+
 class Meta(type):
     def __new__(meta, name, bases, attrs):
         print(Meta, ' __new__')
-        return super(Meta, meta).__new__(meta, name, bases, attrs)
-        #return type.__new__(meta, name, bases, attrs)
+        return type.__new__(meta, name, bases, attrs)
+        # return super(Meta, meta).__new__(meta, name, bases, attrs)
 
     def __init__(cls, name, bases, attrs):
         print(Meta, ' __init__')
-        super(Meta, cls).__init__(name, bases, attrs)
-        #type.__init__(cls, name, bases, attrs)
+        type.__init__(cls, name, bases, attrs)
+        # super(Meta, cls).__init__(name, bases, attrs)
 
     def __call__(cls, *args, **kwargs):
         print(Meta, ' __call__')
-        return super(Meta, cls).__call__(*args, **kwargs)
-        #return type.__call__(cls, *args, **kwargs)
+        return type.__call__(cls, *args, **kwargs)
+        # return super(Meta, cls).__call__(*args, **kwargs)
 
 
 def meta_func(name, bases, attrs):
@@ -40,20 +42,3 @@ class Spam(with_metaclass(Meta, object)):
 print('making instance')
 spam = Spam()
 
-
-class Singleton(type):
-    """Example of singleton"""
-    instance = None
-
-    def __call__(cls, *args, **kwargs):
-        if not cls.instance:
-            cls.instance = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls.instance
-
-
-class ASingleton(with_metaclass(Singleton, object)):
-    pass
-
-a = ASingleton()
-b = ASingleton()
-assert a is b
--- useful_stuff/singleton.py
+++ useful_stuff/singleton.py
@@ -1,37 +1,57 @@
-class Singleton(object):
+def with_metaclass(meta, *bases):
+    """Create a base class with a metaclass."""
+    return meta('NewBase', bases, {})
+
+
+class Meta(type):
+    """Example of singleton meta"""
+    instance = None
+
+    def __call__(cls, *args, **kwargs):
+        if not cls.instance:
+            cls.instance = super(Meta, cls).__call__(*args, **kwargs)
+        return cls.instance
+
+
+class SingletonMeta(with_metaclass(Meta, object)):
+    pass
+
+
+class SingletonClass(object):
+    """Example of singleton class"""
     _instance = None
+
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            cls._instance = super(Singleton, cls).__new__(
-                                cls, *args, **kwargs)
+            cls._instance = super(SingletonClass, cls).__new__(cls, *args, **kwargs)
         return cls._instance
 
 
 def singleton(cls):
+    """Example of singleton decorator"""
     instances = {}
-    def getinstance():
+
+    def get_instance():
         if cls not in instances:
             instances[cls] = cls()
         return instances[cls]
-    return getinstance
+    return get_instance
 
 
 @singleton
-class MyClass:
+class SingletonDecorator:
     pass
 
+
 if __name__ == '__main__':
-    a = Singleton()
-    b = Singleton()
+    a = SingletonMeta()
+    b = SingletonMeta()
     assert a is b
 
-    a = MyClass()
-    b = MyClass()
+    a = SingletonClass()
+    b = SingletonClass()
     assert a is b
 
-
-
-
-
-
-
+    a = SingletonDecorator()
+    b = SingletonDecorator()
+    assert a is b
