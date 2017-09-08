a2547ffe7b047e3a3e002e4110b9ad886699e8d9
Vitalii Zhydkov <vzhidkov@sphereinc.com>
2017-09-08 15:30:41 +0300

    update tests

--- algorithms/sort.py
+++ algorithms/sort.py
@@ -13,36 +13,39 @@ http://bigocheatsheet.com/
 
 def bubble_sort(lst):
     """Return the sorted list
-    >>> bubble_sort([54, 26, 93, 17, 77, 31, 44, 55, 20])
-    [17, 20, 26, 31, 44, 54, 55, 77, 93]
+    >>> lst = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    >>> bubble_sort(lst)
+    >>> lst == [17, 20, 26, 31, 44, 54, 55, 77, 93]
+    True
     """
     l = len(lst)
     for a in range(1, l):
         for b in range(l-1):
             if lst[a] < lst[b]:
                 lst[a], lst[b] = lst[b], lst[a]
-    return lst
 
 
 def selection_sort(lst):
     """Return the sorted list
-    >>> selection_sort([54, 26, 93, 17, 77, 31, 44, 55, 20])
-    [17, 20, 26, 31, 44, 54, 55, 77, 93]
+    >>> lst = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    >>> selection_sort(lst)
+    >>> lst == [17, 20, 26, 31, 44, 54, 55, 77, 93]
+    True
     """
     for i in range(len(lst)-1, 0, -1):
         max_val = 0
         for k in range(1, i+1):
             if lst[k] > lst[max_val]:
                 max_val = k
-
         lst[i], lst[max_val] = lst[max_val], lst[i]
-    return lst
 
 
 def insertion_sort(lst):
     """Return the sorted list
-    >>> insertion_sort([54, 26, 93, 17, 77, 31, 44, 55, 20])
-    [17, 20, 26, 31, 44, 54, 55, 77, 93]
+    >>> lst = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    >>> insertion_sort(lst)
+    >>> lst == [17, 20, 26, 31, 44, 54, 55, 77, 93]
+    True
     """
     for i in range(1, len(lst)):
         val = lst[i]
@@ -51,7 +54,6 @@ def insertion_sort(lst):
             lst[pos] = lst[pos-1]
             pos = pos-1
         lst[pos] = val
-    return lst
 
 
 if __name__ == "__main__":
