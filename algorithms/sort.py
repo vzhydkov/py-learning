0de8ce467b7bafd2ce71051f79d390dc9144ede4
Vitalii Zhydkov <cryptic.kh.ua@gmail.com>
2020-07-11 16:10:05 -0500

    Update sort.py

--- algorithms/sort.py
+++ algorithms/sort.py
@@ -1,13 +1,3 @@
-"""
-A bubble sort, a selection sort, and an insertion sort are O(n**2) algorithms.
-A shell sort improves on the insertion sort by sorting incremental sublists.
-    It falls between O(n) and O(n**2).
-A merge sort is O(n log n), but requires additional space for the merging process.
-A quick sort is O(n log n), but may degrade to O(n**2)
-    if the split points are not near the middle of the list. It does not require additional space.
-"""
-
-
 def bubble_sort(lst):
     """Return the sorted list
     >>> lst = [54, 26, 93, 17, 77, 31, 44, 55, 20]
