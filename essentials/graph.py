05da92fa9830ce2196f5935655fe63dd2297b41d
Vitalii Zhydkov <cryptic.kh.ua@gmail.com>
2020-09-26 10:32:34 -0500

    Update graph.py

--- essentials/graph.py
+++ essentials/graph.py
@@ -1,56 +1,59 @@
-class Graph:
+"""
+Find maximum number of each connected neighbours
+"""
+
+
+class NeighboursGraph:
+    # row and column numbers of 4 neighbours
+    ROW_SHIFT = [0, 1, 0, -1]
+    COL_SHIFT = [-1, 0, 1, 0]
+
     def __init__(self, graph):
+        self.graph = graph
         self.n_row = len(graph)
         self.n_col = len(graph[0])
-        self.graph = graph
-        self.curr_key = None
         self.count = 0
+        self.curr_neighbour = None
 
     def is_safe(self, i, j, visited):
         return (0 <= i < self.n_row
                 and 0 <= j < self.n_col
                 and not visited[i][j]
-                and self.graph[i][j] == self.curr_key)
+                and self.graph[i][j] == self.curr_neighbour)
 
     def dfs(self, i, j, visited):
         self.count += 1
-        # These arrays are used to get row and column numbers of 4 neighbours
-        row_nbr = [0, 1, 0, -1]
-        col_nbr = [-1, 0, 1, 0]
-
         visited[i][j] = True
-        # Recur for all connected neighbours
+        # recur for all connected neighbours
         for k in range(4):
-            if self.is_safe(i + row_nbr[k], j + col_nbr[k], visited):
-                self.dfs(i + row_nbr[k], j + col_nbr[k], visited)
+            if self.is_safe(i + self.ROW_SHIFT[k], j + self.COL_SHIFT[k], visited):
+                self.dfs(i + self.ROW_SHIFT[k], j + self.COL_SHIFT[k], visited)
 
     def count_neighbours(self):
-        """
-        >>> graph.count_neighbours()
-        {0: 3, 1: 5, 2: 1}
-        """
         result = {}
-        check = [self.graph[0][0]]
-        for key in check:
-            self.curr_key = key
+        neighbours = [self.graph[0][0]]
+        for self.curr_neighbour in neighbours:
             visited = [[False for _ in range(self.n_col)] for _ in range(self.n_row)]
             for i in range(self.n_row):
                 for j in range(self.n_col):
-                    if self.graph[i][j] not in check:
-                        check.append(self.graph[i][j])
-                    if visited[i][j] is False and self.graph[i][j] == self.curr_key:
+                    # add new neighbours if exist
+                    if self.graph[i][j] not in neighbours:
+                        neighbours.append(self.graph[i][j])
+                    if visited[i][j] is False and self.graph[i][j] == self.curr_neighbour:
                         self.dfs(i, j, visited)
-                        if result.get(self.curr_key, 0) < self.count:
-                            result[self.curr_key] = self.count
+                        # save only biggest number of neighbours
+                        if result.get(self.curr_neighbour, 0) < self.count:
+                            result[self.curr_neighbour] = self.count
                         self.count = 0
         return result
 
 
 if __name__ == "__main__":
-    graph = [
+    matrix = [
         [0, 0, 1, 2],
         [0, 1, 2, 1],
         [2, 1, 1, 1],
     ]
-    import doctest
-    doctest.testmod(extraglobs={'graph': Graph(graph)})
+    g = NeighboursGraph(matrix)
+    print(g.count_neighbours())
+    # assert {0: 3, 1: 5, 2: 1} == g.count_neighbours()
